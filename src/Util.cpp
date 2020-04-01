44c8e5bacda990606d2f6ac75489fc1ea19ac066
vczn <iamczn.cpp@gmail.com>
2020-04-01 22:09:06 +0800

    add util and unit tests

---
+++ src/Util.cpp
@@ -0,0 +1,25 @@
+#include "Util.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+namespace mdb {
+
+[[noreturn]]
+void mdb_unreachable_internal(const char* msg, const char* filename,
+                              std::uint32_t lineno) {
+#ifndef NDEBUG
+    if (msg) {
+        std::cerr << msg << std::endl;
+    }
+    std::cerr << "Unreachable executed";
+    if (filename) {
+        std::cerr << " at " << filename << ":" << lineno;
+    }
+    std::cerr << "!\n";
+#endif // !NDEBUG
+    abort();
+    MDB_BUILTIN_UNREACHABLE;
+}
+
+} // namespace mdb
