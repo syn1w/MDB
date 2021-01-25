c2ded735c2eafb0fd293e84a1b75c0346b694b29
vczn <iamczn.cpp@gmail.com>
2021-01-25 21:46:02 +0800

    fix unit test

--- test/TestUtil.cpp
+++ test/TestUtil.cpp
@@ -7,7 +7,7 @@ using namespace mdb;
 TEST(UtilTest, test1) {
 #ifndef NDEBUG
     EXPECT_DEATH(MDB_UNREACHABLE("test"),
-                 "test\nUnreachable executed at .*TestUtil.cpp:9");
+                 "test\nUnreachable executed at .*TestUtil.cpp:(9)|(10)");
 #else
     EXPECT_DEATH(MDB_UNREACHABLE("test"), "");
 #endif
